f748520f5fd5e51ee55596fc24d18e5d90aa126e
shilyx <shilyx@outlook.com>
2020-12-04 11:16:41 +0800

    change cpp -> c

--- bm_search_nocase/bm_search_nocase.h
+++ bm_search_nocase/bm_search_nocase.h
@@ -8,9 +8,17 @@
 
 #include <tchar.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 const char* bm_search(const char* str, size_t str_len, const char* needle, size_t needle_len);
 const char* bm_isearch(const char* str, size_t str_len, const char* needle, size_t needle_len);
 const wchar_t* bm_search16(const wchar_t* str, size_t str_len, const wchar_t* needle, size_t needle_len);
 const wchar_t* bm_isearch16(const wchar_t* str, size_t str_len, const wchar_t* needle, size_t needle_len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
