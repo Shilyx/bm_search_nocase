10c659f420e4b6ce173725cf021cb02d23b5448c
shilyx <shilyx@outlook.com>
2020-12-04 11:14:01 +0800

    comment

--- bm_search_nocase/bm_search_nocase.h
+++ bm_search_nocase/bm_search_nocase.h
@@ -1,6 +1,11 @@
 #ifndef _bm_search_nocase_h
 #define _bm_search_nocase_h
 
+// douxian 2020.12.4
+// 使用bm算法支持宽字符搜索，支持大小写匹配搜索
+// 也支持二进制搜索
+// 搜索数据的长度要从外部传入
+
 #include <tchar.h>
 
 const char* bm_search(const char* str, size_t str_len, const char* needle, size_t needle_len);
